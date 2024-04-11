a223d4b81c5b0edbc448728331ae2999d66808d8
blasdo <72580092+blasdo@users.noreply.github.com>
2024-04-11 18:18:39 +0200

    updated philo structure

--- philo.c
+++ philo.c
@@ -6,20 +6,23 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 16:00:51 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/04/11 16:00:52 by bvelasco         ###   ########.fr       */
+/*   Updated: 2024/04/11 18:17:23 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+// begin of philo class
 
 short	p_sleep(t_philo *this, time_t time)
 {
 	int miliseconds;
 
 	miliseconds = get_miliseconds();
-	while (miliseconds < time && miliseconds < *(this->limit_time))
+	while (miliseconds < time
+			&& miliseconds - this->timestamp < *(this->limit_time))
 		miliseconds = get_miliseconds();
-	if (miliseconds >= *(this->limit_time))
+	if (miliseconds - this->timestamp >= *(this->limit_time))
 		return (1);
 	return (0);
 }
@@ -28,17 +31,52 @@ short	eat(t_philo *this)
 {
 	int	miliseconds;
 
+	while (lock_forks(this->forks) != 0)
+	{
+		if (miliseconds  - this->timestamp >= this->limit_time)
+			return 1;
+	}
 	miliseconds = get_miliseconds();
-	if (miliseconds >= *(this->limit_time))
+	if (miliseconds - this->timestamp >= *(this->limit_time))
 		return (1);
 	this->timestamp = get_miliseconds();
 	return (p_sleep(this, *(this->eat_time)));
 }
 
-// not implemented
 void	think(t_philo *this)
 {
-	return;
+	short isalive;
+	
+	isalive = 1;
+	while (isalive)
+	{
+		ft_log(this->philo_id, think);
+		eat(this);
+		p_sleep(this, this->sleep_time);
+	}
+	return ;
 }
-
-t_philo	*new_philo()
+// end of philo class
+void start_philo(t_philo *this)
+{
+	pthread_mutex_lock(this->start_mtx);
+	pthread_mutex_unlock(this->start_mtx);
+	think(this);
+}
+t_philo	*new_philo(t_fork *forks, pthread_mutex_t *mtx[], time_t *c_data[])
+{
+	static __u_int	philo_id = 1;
+	t_philo *philo;
+	
+	philo = malloc(sizeof(t_philo));
+	if (!philo)
+		return (0);
+	philo->philo_id = philo_id;
+	philo->limit_time = c_data[LIMIT_TIME];
+	philo->sleep_time = c_data[SLEEP_TIME];
+	philo->eat_time = c_data[EAT_TIME];
+	philo->forks = forks;
+	philo->log_mtx = mtx[MTX_LOG];
+	pthread_create(philo->thread, NULL, start_philo, philo);
+	return (philo);
+}
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 14:40:30 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/04/11 15:58:27 by bvelasco         ###   ########.fr       */
+/*   Updated: 2024/04/11 18:12:26 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,45 @@
 # define PHILOSOPHERS_H
 # include <pthread.h>
 # include <sys/time.h>
+# define MTX_START 1
+# define MTX_LOG 0
+# define LIMIT_TIME 0
+# define SLEEP_TIME 1
+# define EAT_TIME 2
 
 // non philo class typedefs
-typedef int8_t t_fork;
+typedef struct s_fork
+{
+	pthread_mutex_t *mtx;
+	__int8_t 		islocked;
+}				t_fork;
 // Philo class
 typedef enum e_action
 {
 	THINK,
 	EAT,
-	SLEEP
+	SLEEP,
+	DEAD
 }			t_action;
 
 typedef struct s_philo
 {
 	pthread_t		*thread;
 	time_t			timestamp;
-	time_t			philo_id;
-	t_fork			*forks;
+	__u_int			philo_id;
 	const time_t	*limit_time;
 	const time_t	*sleep_time;
 	const time_t	*eat_time;
-	pthread_mutex_t	*write_mtx;
-	pthread_mutex_t	*dead_mtx;
-	pthread_mutex_t	**forks_mutex;
+	t_fork			*forks;
+	pthread_mutex_t	*log_mtx;
+	pthread_mutex_t *start_mtx;
 }				t_philo;
 
 short	p_sleep(t_philo *this, time_t time);
 short	eat(t_philo *this);
 void	think(t_philo *this);
-t_philo	*new_philo(pthread_mutex_t *write_mtx, pthread_mutex_t *dead,
-			pthread_mutex_t **forks);
+t_philo	*new_philo(t_fork *forks, pthread_mutex_t **mtx, time_t *c_data[]);
 // End of philo class
 time_t	get_miliseconds(void);
-void	ft_log(t_philo *philo, t_action action, pthread_mutex_t *mtx);
+short	ft_log(t_philo *philo, t_action action);
 #endif
